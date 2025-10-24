d625fd77c6dc5a384ef3eed6a9fedd8bfc7a1921
bakkereimarketingofficial <selton@xs4all.nl>
2025-10-24 09:58:38 +0200

    sprint code vernaderd, werkt nogsteets niet

--- Heap_sprint.py
+++ Heap_sprint.py
@@ -1,15 +1,48 @@
-from Heap_sort import min_heapify, arrayinput, txtexport
-
 array = []
 
-while True:
-    arrayinput
-    n = len(array)
-    for i in range(n // 2 - 1, -1, -1):
-        min_heapify(n, i)
-    for i in range(n - 1, 0, -1):
-        array[i], array[0] = array[0], array[i]
-        min_heapify(i, 0)
-    print("Sorted array is:", array)
-    txtexport()
+def arrayinput():
+    """Deze functie vraagt wat de array moet zijn"""
+    while True:
+        print (array)
+        arraynumber = input("add elements to the array and press enter to continue, press enter twice to save the array: ")
+        if arraynumber == "":
+            break
+        elif arraynumber == "stop":
+            break
+        elif arraynumber.startswith ("remove "):
+            try:
+                num_to_remove = int(arraynumber[len("remove "):])
+                if num_to_remove in array:
+                    array.remove(num_to_remove)
+                else:
+                    print("{int(arraynumber)} is not in the array.")
+            except ValueError:
+                print("Please enter a valid integer to remove.")
+        else:
+            try:
+                int(arraynumber)
+                array.append(int(arraynumber))
+            except ValueError:
+                print("Please enter a valid integer, 'stop' to finish or 'remove ...' to remove a number.")
     print("Array saved to heapsortarray.txt")
+
+def min_heapify(n, i):
+    """Deze functie sorteert dingen met een min-heap"""
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+
+    if left < n and array[left] > array[largest]:
+        largest = left
+
+    if right < n and array[right] > array[largest]:
+        largest = right
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+        min_heapify(n, largest)
+
+while True:
+    arrayinput()
+    if array:
+        min_heapify(len(array), 0)
