e6e6664ab9f04d4d327dcde7cc39fed0cbe02a7c
Yannis-Raijmakers <y.raijmakers@ghostrobotics.nl>
2025-10-27 10:46:15 +0100

    begin aan sprint code gemaakt, loop doet het nog niet

--- Heap_sprint.py
+++ Heap_sprint.py
@@ -1,1 +1,22 @@
-f
+array = [6, 7, 4, 1]
+n= len(array)
+i= 0
+while True:
+    def max_heapify(n, i):
+        """Deze functie sorteert dingen met een max-heap"""
+        left = 2 * i + 1
+        right = 2 * i + 2
+        smallest = i
+
+        if left < n and array[left] < array[smallest]:
+            smallest = left
+
+        if right < n and array[right] < array[smallest]:
+            smallest = right
+
+        if smallest != i:
+            array[i], array[smallest] = array[smallest], array[i]
+            max_heapify(n, smallest)
+
+max_heapify(n, i)
+print(array)
