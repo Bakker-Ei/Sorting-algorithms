c333d2206f977b5d2a8a4aaf2f890ba3588667cb
Yannis-Raijmakers <y.raijmakers@ghostrobotics.nl>
2025-10-30 10:25:45 +0100

    Comments toegevoegd om code overzichtelijker te maken

--- Heap_sprint.py
+++ Heap_sprint.py
@@ -11,16 +11,20 @@ i= 0
 
 def min_heapify(n, i):
     """Deze functie sorteert dingen met een min-heap"""
-    largest = i
+    #dit stukje code kijk wat de children zijn met de som, en maakt de parent "largest" {
+    largest = i 
     left = 2 * i + 1
     right = 2 * i + 2
-
+#   }
+    
+    # Dit stukje code kijkt of "largest" ook wel de grootste is, door hem te vergelijken met de twee children ervan 
     if left < n and array[left] > array[largest]:
         largest = left
 
     if right < n and array[right] > array[largest]:
         largest = right
-
+#   }
+    # Als een van de children groter blijkt te zijn dan wisselt hij ze om, en dan kijkt hij daarna naar de children van de omgewisselde
     if largest != i:
         array[i], array[largest] = array[largest], array[i]
         min_heapify(n, largest)
@@ -29,10 +33,10 @@ def min_heapify(n, i):
 while True: 
     n = len(array)
     while True:
-        for i in range(n // 2 - 1, -1, -1):
+        for i in range(n // 2 - 1, -1, -1): #// voor af te ronden naar beneden als N oneven is, loopt alle parents door van onder naar boven
             min_heapify(n, i)
 
-        for i in range(n - 1, 0, -1):
+        for i in range(n - 1, 0, -1): # Pluk het grootste element eruit en voer de code opnieuw uit
             array[i], array[0] = array[0], array[i]
             min_heapify(i, 0)
 
